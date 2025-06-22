b9278160bf935253210561730b011957565815fd
tunir-wabhitkar <24091137@scale.iitrpr.ac.in>
2025-06-22 22:22:39 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true, // recommended for Netlify static export
+
   typescript: {
     ignoreBuildErrors: true,
   },
